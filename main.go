c9eff6e735c98bf0cf1af8d0773dd0fdf3acd2da
AutomaticOrca <liangjiaying1013@gmail.com>
2024-10-25 12:19:11 +1100

    RESTful api using Gin, Loading config with Viper

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"log"
+
+	"github.com/AutomaticOrca/simplebank/api"
+	db "github.com/AutomaticOrca/simplebank/db/sqlc"
+	"github.com/AutomaticOrca/simplebank/util"
+	_ "github.com/lib/pq"
+)
 
 func main() {
-fmt.Println("hello world")
+	config, err := util.LoadConfig(".")
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal(("cannot start server"))
+	}
 }
