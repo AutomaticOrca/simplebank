3debf4e019e58240f1a68449911dc5a8d694ef2e
AutomaticOrca <liangjiaying1013@gmail.com>
2024-10-29 15:46:45 +1100

    implemented login user API and return paseto token

--- api/main_test.go
+++ api/main_test.go
@@ -3,9 +3,26 @@ package api
 import (
 	"os"
 	"testing"
+	"time"
 
+	db "github.com/AutomaticOrca/simplebank/db/sqlc"
+	"github.com/AutomaticOrca/simplebank/util"
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
 )
+
+// create a new server for test
+func newTestServer(t *testing.T, store db.Store) *Server {
+	config := util.Config{
+		TokenSymmetricKey:   util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+
+	server, err := NewServer(config, store)
+	require.NoError(t, err)
+
+	return server
+}
 
 func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
